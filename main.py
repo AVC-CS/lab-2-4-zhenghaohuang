8f86e84b15ea5c17a7b3cbd41a233081b57ecd7a
zhenghaohuang <157772950+zhenghaohuang@users.noreply.github.com>
2024-02-23 03:40:24 +0000

    done

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-
+    sub1 = original_str[:6]
+    sub2 = original_str[7:]
+    merged_str = sub2 + ' ' + sub1 
     print(sub2)
     print(sub1)
     print(merged_str)
-
+    
     #########################################
     # Do not delete the return statement
     return sub1, sub2, merged_str
 
 
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
